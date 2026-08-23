be666a078e0e55b4d11ca0ded78cf907ced7715b
hbluecat706 <hbluecat706@gmail.com>
2026-08-23 04:01:42 +0330

    Delete build-worker-entry.mjs

--- build-worker-entry.mjs
+++
@@ -1,5 +0,0 @@
-import { writeFileSync } from "node:fs";
-
-const entry = `export { default } from "./worker.source.mjs";\nexport * from "./worker.source.mjs";\n`;
-writeFileSync(new URL("./worker.js", import.meta.url), entry, "utf8");
-console.log("worker.js module entry: ok");
